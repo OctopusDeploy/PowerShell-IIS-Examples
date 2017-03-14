fcc7598fe9cc8c3095ade291b6eb234f145c0fc9
Paul Stovell <paul@paulstovell.com>
2017-03-14 15:02:47 +1000

    Creating app pools with common options

---
+++ examples/03-CreateAppPool/IIS.ps1
@@ -0,0 +1,77 @@
+# -----------------------------------------------------------------------------
+# Setup
+# -----------------------------------------------------------------------------
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $here
+
+. ..\_Setup.IIS.ps1
+
+mkdir "C:\Sites\Website1" -ErrorAction SilentlyContinue
+
+New-IISSite -Name "Website1" -BindingInformation "*:8022:" -PhysicalPath "C:\Sites\Website1"
+
+# -----------------------------------------------------------------------------
+# Example
+# -----------------------------------------------------------------------------
+Import-Module IISAdministration
+
+$pool = Get-IISAppPool -Name "My Pool 3" -WarningAction SilentlyContinue
+if ($pool -eq $null) {
+    $manager = Get-IISServerManager
+    $pool = $manager.ApplicationPools.Add("My Pool 3")
+
+    # Older applications may require "Classic" mode, but most modern ASP.NET
+    # apps use the integrated pipeline
+    $pool.ManagedPipelineMode = "Integrated"
+
+    # What version of the .NET runtime to use. Valid options are "v2.0" and 
+    # "v4.0". IIS Manager often presents them as ".NET 4.5", but these still 
+    # use the .NET 4.0 runtime so should use "v4.0". 
+    $pool.ManagedRuntimeVersion = "v4.0"
+
+    # If your ASP.NET app must run as a 32-bit process even on 64-bit machines
+    # set this to $true. This is usually only important if your app depends 
+    # on some unmanaged (non-.NET) DLL's. 
+    $pool.Enable32BitAppOnWin64 = $false
+
+    # Starts the application pool automatically when a request is made. If you 
+    # set this to false, you have to manually start the application pool or 
+    # you will get 503 errors. 
+    $pool.AutoStart = $true
+
+    # "AlwaysRunning" = application pool loads when Windows starts, stays running
+    # even when the application/site is idle. 
+    # "OnDemand" = IIS starts it when needed. If there are no requests, it may 
+    # never be started. 
+    $pool.StartMode = "OnDemand"
+
+    # What account does the application pool run as? 
+    # "ApplicationPoolIdentity" = best
+    # "LocalSysten" = bad idea!
+    # "NetworkService" = not so bad
+    # "SpecificUser" = useful if the user needs special rights
+    $pool.ProcessModel.IdentityType = "ApplicationPoolIdentity"
+
+    $manager.CommitChanges()
+}
+
+$website = Get-IISSite -Name "Website1"
+$website.Applications["/"].ApplicationPoolName = "My Pool 3"
+
+# -----------------------------------------------------------------------------
+# Assert
+# -----------------------------------------------------------------------------
+
+if ((Get-IISAppPool -Name "My Pool 3") -eq $null) { Write-Error "My Pool not found" }
+
+# -----------------------------------------------------------------------------
+# Clean up
+# -----------------------------------------------------------------------------
+
+. ..\_Teardown.IIS.ps1
+
+Remove-IISSite -Name "Website1" -Confirm:$false
+
+$manager = Get-IISServerManager
+$manager.ApplicationPools["My Pool 3"].Delete()
+$manager.CommitChanges()
---
+++ examples/03-CreateAppPool/Web.ps1
@@ -0,0 +1,70 @@
+# -----------------------------------------------------------------------------
+# Setup
+# -----------------------------------------------------------------------------
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $here
+
+. ..\_Setup.Web.ps1
+
+mkdir "C:\Sites\Website1" -ErrorAction SilentlyContinue
+
+New-Website -Name "Website1" -Port 80 -IPAddress "*" -HostHeader "" -PhysicalPath "C:\Sites\Website1"
+
+# -----------------------------------------------------------------------------
+# Example
+# -----------------------------------------------------------------------------
+
+Import-Module WebAdministration
+
+New-Item -Path "IIS:\AppPools" -Name "My Pool 3" -Type AppPool
+
+# Older applications may require "Classic" mode, but most modern ASP.NET
+# apps use the integrated pipeline
+Set-ItemProperty -Path "IIS:\AppPools\My Pool 3" -name "managedPipelineMode" -value "Integrated"
+
+# What version of the .NET runtime to use. Valid options are "v2.0" and 
+# "v4.0". IIS Manager often presents them as ".NET 4.5", but these still 
+# use the .NET 4.0 runtime so should use "v4.0". 
+Set-ItemProperty -Path "IIS:\AppPools\My Pool 3" -name "managedRuntimeVersion" -value "v4.0"
+
+# If your ASP.NET app must run as a 32-bit process even on 64-bit machines
+# set this to $true. This is usually only important if your app depends 
+# on some unmanaged (non-.NET) DLL's. 
+Set-ItemProperty -Path "IIS:\AppPools\My Pool 3" -name "enable32BitAppOnWin64" -value $false
+
+# Starts the application pool automatically when a request is made. If you 
+# set this to false, you have to manually start the application pool or 
+# you will get 503 errors. 
+Set-ItemProperty -Path "IIS:\AppPools\My Pool 3" -name "autoStart" -value $true
+
+# "AlwaysRunning" = application pool loads when Windows starts, stays running
+# even when the application/site is idle. 
+# "OnDemand" = IIS starts it when needed. If there are no requests, it may 
+# never be started. 
+Set-ItemProperty -Path "IIS:\AppPools\My Pool 3" -name "startMode" -value "OnDemand"
+
+# What account does the application pool run as? 
+# "ApplicationPoolIdentity" = best
+# "LocalSysten" = bad idea!
+# "NetworkService" = not so bad
+# "SpecificUser" = useful if the user needs special rights
+Set-ItemProperty -Path "IIS:\AppPools\My Pool 3" -name "processModel.identityType" -value "ApplicationPoolIdentity"
+
+# Assign application pool to website
+Set-ItemProperty -Path "IIS:\Sites\Website1" -name "applicationPool" -value "My Pool 3"
+
+# -----------------------------------------------------------------------------
+# Assert
+# -----------------------------------------------------------------------------
+
+if ((Get-WebAppPoolState -Name "My Pool 3") -eq $null) { Write-Error "Website1 not found" }
+if ((Get-WebSite -Name "Website1") -eq $null) { Write-Error "Website1 not found" }
+
+# -----------------------------------------------------------------------------
+# Clean up
+# -----------------------------------------------------------------------------
+
+. ..\_Teardown.Web.ps1
+
+Remove-Item -Path "IIS:\Sites\Website1" -Recurse -Force
+Remove-Item -Path "IIS:\AppPools\My Pool 3" -Recurse -Force
