107cfe88069dcfc28a3e656f9c1cb41f0522da85
Paul Stovell <paul@paulstovell.com>
2017-03-15 14:37:03 +1000

    Add virtual directory example

---
+++ examples/04-CreateVDir/Web.ps1
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------------------------
+# Setup
+# -----------------------------------------------------------------------------
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $here
+
+. ..\_Setup.Web.ps1
+
+mkdir "C:\Sites\Website1" -ErrorAction SilentlyContinue
+mkdir "C:\Sites\MyApp" -ErrorAction SilentlyContinue
+
+New-Website -Name "Website1" -Port 80 -IPAddress "*" -HostHeader "" -PhysicalPath "C:\Sites\Website1"
+
+# -----------------------------------------------------------------------------
+# Example
+# -----------------------------------------------------------------------------
+
+Import-Module WebAdministration
+
+New-Item -Type Application -Path "IIS:\Sites\Website1\MyApp" -physicalPath "C:\Sites\MyApp"
+Set-ItemProperty -Path "IIS:\Sites\Website1\MyApp" -name "applicationPool" -value ".NET v2.0"
+
+# -----------------------------------------------------------------------------
+# Assert
+# -----------------------------------------------------------------------------
+
+if ((Test-Path IIS:\Sites\Website1\MyApp) -eq $false) { Write-Error "App not found" }
+if ((Get-ItemProperty -Path "IIS:\Sites\Website1\MyApp" -name "applicationPool") -ne ".NET v2.0") { Write-Error "Wrong app pool" }
+
+# -----------------------------------------------------------------------------
+# Clean up
+# -----------------------------------------------------------------------------
+
+. ..\_Teardown.Web.ps1
+
+Remove-Item -Path "IIS:\Sites\Website1" -Recurse -Force
