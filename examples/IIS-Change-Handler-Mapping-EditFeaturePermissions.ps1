9dda8e7b3e401d6bd442a3f2c75ffc2a9588c276
danielfischeroctopus <daniel.fischer@octopus.com>
2017-06-08 13:04:36 +1000

    Create IIS-Change-Handler-Mapping-EditFeaturePermissions.ps1
    This script will change the Handler Mappings "Feature Permissions" select from Read, Script, Execute

---
+++ examples/IIS-Change-Handler-Mapping-EditFeaturePermissions.ps1
@@ -0,0 +1,17 @@
+#Script to edit the feature permissions for individual Site Handler Mappings in IIS
+
+Import-Module WebAdministration
+
+#Set location of your sites. 
+$site = "IIS:\Sites"
+
+#enter the setting for the "Edit Feature Permissions" Read, Script, Execute    -   Execute require script.
+$setpolicy = "Read, Script"
+
+#Sets the Handler Mapping feature delegation to Read/Write
+Set-WebConfiguration //System.webServer/handlers -metadata overrideMode -value Allow -PSPath IIS:/ -verbose
+
+Set-WebConfiguration -filter "/system.webServer/handlers/@AccessPolicy" -PSPath $site -value $setpolicy -verbose
+
+#Sets the Handler Mapping feature delegation to Read Only. (Left commented out by default)
+#Set-WebConfiguration //System.webServer/handlers -metadata overrideMode -value Deny -PSPath IIS:/ -verbos
